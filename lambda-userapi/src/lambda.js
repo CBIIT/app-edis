ca0d1405b92093165ee85ea1910ca9f3daff14aa
polonskyy <yakov.polonsky@nih.gov>
2021-06-25 15:04:27 -0400

    testing lambda code change

--- lambda-userapi/src/lambda.js
+++ lambda-userapi/src/lambda.js
@@ -8,6 +8,7 @@ if (logLevel && logLevel == 'info') {
 
 // app.register(require('./restapi'), { prefix: '/nedorgapi/v1'})
 app.register(require('./eracommonsapi'), { prefix: '/eracommonsapi/v1'})
+console.debug('The application has been registered');
 
 module.exports.handler = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false
