8eb48886b81c33c0ac2143380abe1e203f2e5136
polonskyy <yakov.polonsky@nih.gov>
2021-06-25 14:23:50 -0400

    modify scripts and added zip files for ci/cd process

--- install-scripts/exec-aws.sh
+++ install-scripts/exec-aws.sh
@@ -1,110 +1,63 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-a <account>] [-t <tier>] [-p profile] [-h]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-a <s3bucket>] [-t <tier>] [-p profile] [-h]" 1>&2; exit 1; }
 
-account="107424568411"
+s3bucket="cf-templates-107424568411-us-east-1"
 tier="dev"
 profile="default"
-w_ec2="n"
 cf_dir="../aws-cf-scripts"
 
-while getopts ha:t:e:p: opt
+while getopts ha:t:p: opt
 do
     case "${opt}" in
         h) usage
           ;;
-        a) account=${OPTARG}
+        a) s3bucket=${OPTARG}
           ;;
         t) tier=${OPTARG}
           ;;
         p) profile=${OPTARG}
-          ;;
-        e)
-          if [[ ${OPTARG} = "yes" || ${OPTARG} = "y" ]]
-          then w_ec2="y"
-          else w_ec2="n"
-          fi
           ;;
         *) usage
           ;;
     esac
 done
 
-s3bucket="cf-templates-$account-us-east-1"
 
-cf_ec2="ec2-serverless-template.yaml"
 cf_ddb="ddb-serverless-template.yaml"
 cf_iam_lambda="iam-lambda-role-template.yaml"
 cf_iam_apigateway="iam-apigtw-role-template.yaml"
-#cf_ec2_sg="ec2-sg-template.yaml"
-cf_vpc="vpc-endpoint-template.yaml"
-keypair=../pgmSSHKey.pem
 
-echo "account = $account, s3 = ${s3bucket}"
-echo ""
-s3exist=$(aws s3api list-buckets --profile ${profile} --query "Buckets[].Name" | grep ${s3bucket})
-if [[ -z ${s3exist} ]]
-then
-  echo Create a new bucket ${s3bucket}
-  aws s3api create-bucket --profile ${profile} --bucket "$s3bucket" --region us-east-1
+echo -e "Create ${s3bucket} if it does not exist"
+
+s3exist=$(aws s3api head-bucket --bucket $s3bucket --profile ${profile} 2>&1 || true)
+if [ -n "${s3exist}" ]
+ then
+    echo -e "Bucket ${s3bucket} does not exist. Creating a new one"
+    aws s3api create-bucket --profile ${profile} --bucket "$s3bucket" --region us-east-1
 fi
 
-if [[ ${w_ec2} = "y" ]]
-then
-  aws s3 cp "$cf_ec2" "s3://$s3bucket"
-fi
-aws s3 cp  --profile ${profile} "$cf_dir/$cf_ddb" "s3://$s3bucket"
-aws s3 cp  --profile ${profile} "$cf_dir/$cf_iam_lambda" "s3://$s3bucket"
-aws s3 cp  --profile ${profile} "$cf_dir/$cf_iam_apigateway" "s3://$s3bucket"
-#aws s3 cp  --profile ${profile} "$cf_dir/$cf_ec2_sg" "s3://$s3bucket"
-aws s3 cp  --profile ${profile} "$cf_dir/$cf_vpc" "s3://$s3bucket"
+aws cloudformation deploy --profile ${profile} --stack-name ${tier}-userapi-iam-lambda --template-file "$cf_dir/$cf_iam_lambda" --s3-bucket ${s3bucket} --parameter-overrides \
+        Environment=${tier} --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --profile ${profile} --stack-name ${tier}-userapi-iam-apigtwy --template-file "$cf_dir/$cf_iam_apigateway" --s3-bucket ${s3bucket} --parameter-overrides \
+        Environment=${tier} --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --profile ${profile} --stack-name ${tier}-userapi-ddb  --template-file "$cf_dir/$cf_ddb" --s3-bucket ${s3bucket} --parameter-overrides \
+        Environment=${tier}
 
-#get current VPC id
-vpcid=$(aws ec2 describe-vpcs --profile ${profile} --query Vpcs[0].VpcId | sed -e 's/^"//' -e 's/"$//')
-echo VPC id = $vpcid
-
-#get us-east-1a Subnet id
-subnetid=$(aws ec2 describe-subnets  --profile ${profile} --filters "Name=availability-zone,Values=us-east-1a" --query "Subnets[0].SubnetId" | sed -e 's/^"//' -e 's/"$//')
-echo Subnet id = $subnetid
-
-#get default VPC security group
-sg=$(aws ec2 describe-security-groups  --profile ${profile} --query "SecurityGroups[?GroupName=='default'].GroupId | [0]" | sed -e 's/^"//' -e 's/"$//')
-echo Default Security Group = $sg
-
-
-aws cloudformation create-stack --profile ${profile} --stack-name ${tier}-userapi-iam-lambda --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_iam_lambda}" --parameters ParameterKey=Environment,ParameterValue=${tier} --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --profile ${profile}  --stack-name ${tier}-userapi-iam-apigtwy --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_iam_apigateway}" --parameters ParameterKey=Environment,ParameterValue=${tier} --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --profile ${profile}  --stack-name ${tier}-userapi-ddb --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_ddb}" --parameters ParameterKey=Environment,ParameterValue=${tier}
-#aws cloudformation create-stack --profile ${profile}  --stack-name ${tier}-userapi-ec2-sg --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_ec2_sg}" --parameters ParameterKey=VpcId,ParameterValue=$vpcid ParameterKey=SubnetId,ParameterValue=$subnetid ParameterKey=VpcDefaultSgId,ParameterValue=$sg
-#aws cloudformation create-stack --stack-name ${tier}-userapi-vpc-endpoint --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_vpc}" --parameters ParameterKey=VpcId,ParameterValue=$vpcid ParameterKey=SubnetId,ParameterValue=$subnetid
-
-arnddb=null
-while [ ${arnddb} = null ]; do
+stackStatus=null
+echo -n "Creating stacks..."
+while [ ${stackStatus} != 'CREATE_COMPLETE' ] && [ ${stackStatus} != 'ROLLBACK_COMPLETE' ] && [ ${stackStatus} != 'CREATE_FAILED' ] && [ ${stackStatus} != 'UPDATE_COMPLETE' ] && [ ${stackStatus} != 'UPDATE_FAILED' ]; do
     sleep 2s
     echo -n "."
-    arnddb=$(aws cloudformation describe-stacks  --profile ${profile} --stack-name ${tier}-userapi-ddb --query "Stacks[0].Outputs[?OutputKey=='DdbExtusersArn'].OutputValue | [0]" | sed -e 's/^"//' -e 's/"$//')
+    stackStatus=$(aws cloudformation describe-stacks --stack-name ${tier}-userapi-ddb --query "Stacks[0].StackStatus" | sed -e 's/^"//' -e 's/"$//')
 done
 
-if [[ ${w_ec2} = "y" ]]
+if [ ${stackStatus} = 'CREATE_COMPLETE' ]
 then
-  rm -f $keypair
-  aws ec2 create-key-pair  --profile ${profile} --key-name pgmSSHKey --query 'KeyMaterial' --output text >$keypair
-  chmod 400 $keypair
-
-  aws cloudformation create-stack  --profile ${profile} --stack-name ${tier}-ec2-serverless --template-url "https://${s3bucket}.s3.amazonaws.com/${cf_ec2}" --parameters ParameterKey=VpcId,ParameterValue=$vpcid ParameterKey=SubnetId,ParameterValue=$subnetid ParameterKey=WebSGId,ParameterValue=$sgid
-  echo -n "Creating stacks..."
-  webserver=null
-  while [ ${webserver} == null ]; do
-      sleep 2s
-      echo -n "."
-      webserver=$(aws cloudformation describe-stacks  --profile ${profile} --stack-name ${dev}-ec2-serverless --query "Stacks[0].Outputs[?OutputKey=='WebServer'].OutputValue | [0]")
-  done
-  echo -e "\nWeb server has been created."
-  echo -e "\nTo SSH to the web server use the following command:"
-  echo -e "      ssh -i \"$keypair\" ec2-user@$webserver"
-  echo -e "To copy file to the web server use the following command:"
-  echo -e "      scp -i \"$keypair\" <local file> ec2-user@$webserver:<remote folder>"
+  echo -e "\nCloudFormation stacks have been created successfully."
+elif [ ${stackStatus} = 'UPDATE_COMPLETE' ]
+then
+  echo -e "\nCloudFormation stacks have been updated successfully."
 else
-  echo -e "\nCloud Formation Stacks have been deployed"
+  echo -e "\nFAILED: Failed to create or update CloudFormation stacks.  Please, check the CloudFormation stacks in AWS Console."
 fi
-
