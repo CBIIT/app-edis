19ff94c972ea9b3f2ef0e12bc4025fce9fa154fa
polonskyy <yakov.polonsky@nih.gov>
2021-08-19 11:49:50 -0400

    prepared scripts without profile

---
+++ install-scripts/exec-aws-no-profile.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-a <s3bucket>] [-t <tier>] [-h]" 1>&2; exit 1; }
+
+s3bucket="cf-templates-107424568411-us-east-1"
+tier="dev"
+cf_dir="../aws-cf-scripts"
+
+while getopts ha:t: opt
+do
+    case "${opt}" in
+        h) usage
+          ;;
+        a) s3bucket=${OPTARG}
+          ;;
+        t) tier=${OPTARG}
+          ;;
+        *) usage
+          ;;
+    esac
+done
+
+
+cf_ddb="ddb-serverless-template.yaml"
+cf_iam_lambda="iam-lambda-role-template.yaml"
+cf_iam_apigateway="iam-apigtw-role-template.yaml"
+
+s3exist=$(aws s3api head-bucket --bucket $s3bucket 2>&1 || true)
+if [ -n "${s3exist}" ] 
+ then
+    echo -e "ERROR: Bucket ${s3bucket} does not exist."
+    exit 1
+fi
+
+aws cloudformation deploy --stack-name ${tier}-edis-iam-lambda --template-file "$cf_dir/$cf_iam_lambda" \
+                          --s3-bucket ${s3bucket} --s3-prefix app-edis-${tier} \
+                          --parameter-overrides \
+                            Environment=${tier} --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation deploy --stack-name ${tier}-edis-iam-apigtwy --template-file "$cf_dir/$cf_iam_apigateway" \
+                          --s3-bucket ${s3bucket} --s3-prefix app-edis-${tier} \
+                          --parameter-overrides \
+                            Environment=${tier} --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation deploy --stack-name ${tier}-edis-ddb  --template-file "$cf_dir/$cf_ddb" \
+                          --s3-bucket ${s3bucket} --s3-prefix app-edis-${tier} \
+                          --parameter-overrides \
+                            Environment=${tier}
+
+stackStatus=$(aws cloudformation describe-stacks --stack-name ${tier}-edis-ddb --query "Stacks[0].StackStatus" | sed -e 's/^"//' -e 's/"$//')
+if [ "$stackStatus" = 'CREATE_COMPLETE' ]
+then
+  echo -e "\nCloudFormation stacks have been created successfully."
+elif [ "$stackStatus" = 'UPDATE_COMPLETE' ]
+then
+  echo -e "\nCloudFormation stacks have been updated successfully."
+else
+  echo -e "\nFAILED: Failed to create or update CloudFormation stacks.  Please, check the CloudFormation stacks in AWS Console."
+  exit 1
+fi
---
+++ install-scripts/sam-deploy-no-profile.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 [-a <s3bucket>] [-t <tier>] [-h]" 1>&2; exit 1; }
+
+s3bucket="cf-templates-107424568411-us-east-1"
+tier="dev"
+cf_dir="../aws-cf-scripts"
+
+while getopts ha:t:p: opt
+do
+    case "${opt}" in
+        h) usage
+          ;;
+        a) s3bucket=${OPTARG}
+          ;;
+        t) tier=${OPTARG}
+          ;;
+        *) usage
+          ;;
+    esac
+done
+
+sam_template="sam-openapi-template.yaml"
+
+s3prefix="app-edis-${tier}"
+region="us-east-1"
+capabilities="CAPABILITY_IAM"
+
+lambda_role_arn=$(aws cloudformation describe-stacks  --stack-name ${tier}-edis-iam-lambda --query "Stacks[0].Outputs[?OutputKey=='LambdaOrgapiRoleArn'].OutputValue | [0]" | sed -e 's/^"//' -e 's/"$//')
+dynamodb_role_arn=$(aws cloudformation describe-stacks  --stack-name ${tier}-edis-iam-apigtwy --query "Stacks[0].Outputs[?OutputKey=='ApiGatewayAccessDdbRoleArn'].OutputValue | [0]" | sed -e 's/^"//' -e 's/"$//')
+
+echo -e "Parameters: $tier $lambda_role_arn"
+
+sam deploy -t $cf_dir/$sam_template --stack-name ${tier}-edis-app-serverless --s3-bucket $s3bucket --s3-prefix $s3prefix \
+                                    --region $region --no-confirm-changeset --capabilities $capabilities \
+                                    --parameter-overrides \
+                    Environment=$tier \
+                    LambdaRoleArn=$lambda_role_arn \
+                    DynamoDbRoleArn=$dynamodb_role_arn \
+                    S3bucket=$s3bucket \
+                    UsersTableName=extusers-$tier \
+                    Issuer=https://iam-lab2.cancer.gov/oauth2/auss3iezeLBILuhGa1d6 \
+                    Audience=api://default
+
+echo -e "\nServerless Cloud Formation Stack has been deployed"
