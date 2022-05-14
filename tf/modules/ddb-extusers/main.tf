9deafba2b1a42a9ff579163dcd6167a304954048
Cole DeVries <colemandevries@gmail.com>
2022-05-14 12:37:12 -0400

    making dynamodb more generic in conventions and variable usage

--- tf/modules/ddb-extusers/main.tf
+++ tf/modules/ddb-extusers/main.tf
@@ -1,11 +1,18 @@
 
 resource "aws_dynamodb_table" "dynamodb" {
-  name           = "${var.app}-dynamodb-table-${var.env}"
-  hash_key       = var.dynamodb_hash_key
-  billing_mode   = var.dynamodb_billing_mode
-  read_capacity  = var.dynamodb_read_capacity
-  write_capacity = var.dynamodb_write_capacity
+  name             = "${var.app}-dynamodb-table-${var.env}"
+  hash_key         = var.dynamodb_hash_key
+  billing_mode     = var.dynamodb_billing_mode
+  read_capacity    = var.dynamodb_read_capacity
+  write_capacity   = var.dynamodb_write_capacity
+  stream_enabled   = var.dynamodb_stream_enabled
+  stream_view_type = var.dynamodb_stream_view_type
+  table_class      = var.dynamodb_table_class
 
+  ttl {
+    enabled        = var.dynamodb_ttl_enabled
+    attribute_name = "ttl"
+  }
 
   attribute {
     name = "USER_ID"
--- tf/modules/ddb-extusers/variables.tf
+++ tf/modules/ddb-extusers/variables.tf
@@ -24,6 +24,28 @@ variable "dynamodb_write_capacity" {
   default = 5
 }
 
+variable "dynamodb_ttl_enabled" {
+  type        = bool
+  description = ""
+  default     = false
+}
+
+variable "dynamodb_stream_enabled" {
+  type    = bool
+  default = false
+}
+
+variable "dynamodb_stream_view_type" {
+  type        = string
+  description = "When an item in the table is modified, StreamViewType determines what information is written to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES."
+}
+
+variable "dynamodb_table_class" {
+  type        = string
+  description = "The storage class of the table. Valid values are STANDARD and STANDARD_INFREQUENT_ACCESS."
+  default     = "STANDARD"
+}
+
 variable "must-be-role-prefix" {
   type        = string
   description = "Mandatory IAM role name prefix"
