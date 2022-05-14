81acf06995ecbbef707497b011b8d3e13f1acc7f
Cole DeVries <colemandevries@gmail.com>
2022-05-14 16:38:17 -0400

    restructuring api-gateway module/file composition

--- tf/modules/api-gateway/main.tf
+++ tf/modules/api-gateway/main.tf
@@ -104,3 +104,37 @@ resource "aws_cloudwatch_log_group" "api_gateway" {
   name              = "${var.portfolio}-${var.env}-${var.app}-accesslogs"
   retention_in_days = var.cloudwatch_log_retention_days
 }
+
+resource "aws_iam_role" "auth_lambda" {
+  name                 = "${var.must-be-role-prefix}-lambda-auth-${var.env}"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role_lambda_service.json
+  path                 = "/"
+  permissions_boundary = var.must-be-policy-arn
+}
+
+resource "aws_iam_role_policy_attachment" "auth_lambda" {
+  role       = aws_iam_role.auth_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "auth_lambda" {
+  role       = aws_iam_role.auth_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "auth_lambda" {
+  role       = aws_iam_role.auth_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
+
+resource "aws_iam_role" "api_gateway" {
+  name                 = "${var.must-be-role-prefix}-api-gateway-${var.app}-${var.env}"
+  assume_role_policy   = data.aws_iam_policy_document.assume_role_apigateway_service
+  path                 = "/"
+  permissions_boundary = var.must-be-policy-arn
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway" {
+  role       = aws_iam_role.api_gateway.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
