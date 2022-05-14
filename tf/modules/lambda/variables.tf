2d0f9754b6ee80c59a58493fafa28b17d21de5fc
Cole DeVries <colemandevries@gmail.com>
2022-05-14 11:48:43 -0400

    Updated Lambda module main.tf and variables.tf

--- tf/modules/lambda/variables.tf
+++ tf/modules/lambda/variables.tf
@@ -1,3 +1,41 @@
+variable "app" {
+  type        = string
+  description = "Provide the name of the application or the service"
+}
+
+variable "lambda_description" {
+  type        = string
+  description = "Provide a description for the lambda function"
+}
+
+variable "lambda_handler_file" {
+  type        = string
+  description = "The source location for the lambda handler definition (i.e. src/handler.lambda)"
+}
+
+variable "lambda_runtime" {
+  type        = string
+  description = "The runtime for the lambda function. The default value represents the latest runtime version validated for this configuration"
+  default     = "nodejs12.x"
+}
+
+variable "lambda_memory_size" {
+  type        = number
+  description = "The memory allocation for the lambda function"
+  default     = 2048
+}
+
+variable "lambda_timeout" {
+  type        = number
+  description = "The lambda function timeout duration"
+  default     = 30
+}
+
+variable "lambda_file_location" {
+  type        = string
+  description = "Location of the file that contains the lambda function code (i.e. '../lambda-zip/lambda-userapi/lambda-userapi.zip')"
+}
+
 variable "env" {
   type        = string
   description = "The target environment for the deployment"
