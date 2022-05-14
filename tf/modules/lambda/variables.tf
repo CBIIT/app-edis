0680ce20528d3355c4e75dd346d24d27fa318a28
Cole DeVries <colemandevries@gmail.com>
2022-05-14 12:04:58 -0400

    finished refactoring lambda module, created outputs file

--- tf/modules/lambda/variables.tf
+++ tf/modules/lambda/variables.tf
@@ -21,7 +21,7 @@ variable "lambda_runtime" {
 
 variable "lambda_memory_size" {
   type        = number
-  description = "The memory allocation for the lambda function"
+  description = "Amount of memory in MB allocated for your Lambda Function used at runtime"
   default     = 2048
 }
 
@@ -33,7 +33,24 @@ variable "lambda_timeout" {
 
 variable "lambda_file_location" {
   type        = string
-  description = "Location of the file that contains the lambda function code (i.e. '../lambda-zip/lambda-userapi/lambda-userapi.zip')"
+  description = "Path to the function's deployment package within the local filesystem (i.e. '../lambda-zip/lambda-userapi/lambda-userapi.zip')"
+}
+
+variable "lambda_log_level" {
+  type        = string
+  description = "Set the log level for your Lambda Function"
+  default     = "info"
+}
+
+variable "lambda_tracing_mode" {
+  type        = string
+  description = "Whether to to sample and trace a subset of incoming requests with AWS X-Ray. Valid values are PassThrough and Active. If PassThrough, Lambda will only trace the request from an upstream service if it contains a tracing header with 'sampled=1'. If Active, Lambda will respect any tracing header it receives from an upstream service. If no tracing header is received, Lambda will call X-Ray for a tracing decision."
+}
+
+variable "lambda_config_retry_attempts" {
+  type        = number
+  description = "Maximum number of times to retry when the function returns an error. Valid values between 0 and 2."
+  default     = 0
 }
 
 variable "env" {
