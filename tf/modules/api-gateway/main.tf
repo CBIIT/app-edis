2216bf0c1f1c7538ecc999cadabe50f44f192d86
Cole DeVries <colemandevries@gmail.com>
2022-05-14 16:11:43 -0400

    format changes for readability

--- tf/modules/api-gateway/main.tf
+++ tf/modules/api-gateway/main.tf
@@ -84,14 +84,14 @@ resource "aws_api_gateway_stage" "api_gateway" {
     destination_arn = aws_cloudwatch_log_group.api_gateway.arn
     format          = "{ \"requestTime\":  \"$context.requestTime\", \"requestId\": \"$context.requestId\", \"httpMethod\": \"$context.httpMethod\", \"path\": \"$context.path\", \"resourcePath\": \"$context.resourcePath\", \"status\": $context.status, \"responseLatency\": $context.responseLatency, \"xrayTraceId\": \"$context.xrayTraceId\", \"integrationRequestId\": \"$context.integration.requestId\", \"functionResponseStatus\": \"$context.integration.status\", \"integrationLatency\": \"$context.integration.latency\", \"integrationServiceStatus\": \"$context.integration.integrationStatus\", \"authorizeResultStatus\": \"$context.authorize.status\", \"authorizerServiceStatus\": \"$context.authorizer.status\", \"authorizerLatency\": \"$context.authorizer.latency\", \"authorizerRequestId\": \"$context.authorizer.requestId\", \"ip\": \"$context.identity.sourceIp\", \"userAgent\": \"$context.identity.userAgent\", \"principalId\": \"$context.authorizer.principalId\", \"user\": \"$context.identity.user\" }"
   }
-  xray_tracing_enabled = true
+  xray_tracing_enabled = var.apigw_stage_xray_enabled
 }
 
 resource "aws_api_gateway_method_settings" "api_gateway" {
   method_path = var.apigw_method_path
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   stage_name  = aws_api_gateway_stage.api_gateway.stage_name
-  
+
   settings {
     metrics_enabled      = var.apigw_method_metrics_enabled
     logging_level        = var.apigw_method_log_level
