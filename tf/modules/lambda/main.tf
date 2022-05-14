db18eb17e7974f313a60f2c94691c4e5b2b2c555
Cole DeVries <colemandevries@gmail.com>
2022-05-14 16:47:15 -0400

    updated lambda module file structure

--- tf/modules/lambda/main.tf
+++ tf/modules/lambda/main.tf
@@ -29,3 +29,28 @@ resource "aws_lambda_function_event_invoke_config" "lambda" {
   function_name          = aws_lambda_function.lambda.function_name
   maximum_retry_attempts = var.lambda_config_retry_attempts
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "${var.must-be-role-prefix}-lambda-user-api-${var.env}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+  ]
+  path                 = "/"
+  permissions_boundary = var.must-be-policy-arn
+}
