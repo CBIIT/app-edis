19af06ab1bf8523392e78668940d23e9b6e3079c
Cole DeVries <colemandevries@gmail.com>
2022-05-14 13:10:19 -0400

    modifying dynamo role and policies with aws_iam_policy_document data resource

---
+++ tf/modules/ddb-extusers/data.tf
@@ -0,0 +1,57 @@
+data "aws_iam_policy_document" "dynamodb_access" {
+  statement {
+
+    sid = "DynamoDbAccess"
+
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:BatchWriteItem",
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:DescribeTable",
+      "dynamodb:DescribeTimeToLive",
+      "dynamodb:GetItem",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams",
+      "dynamodb:ListTables",
+      "dynamodb:ListTagsOfResource",
+      "dynamodb:PartiQLDelete",
+      "dynamodb:PartiQLInsert",
+      "dynamodb:PartiQLSelect",
+      "dynamodb:PartiQLUpdate",
+      "dynamodb:PutItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:TagResource",
+      "dynamodb:UntagResource",
+      "dynamodb:UpdateItem",
+      "dynamodb:UpdateTable",
+      "dynamodb:UpdateTimeToLive"
+    ]
+
+    resources = [
+      aws_dynamodb_table.dynamodb.arn,
+      "${aws_dynamodb_table.dynamodb.arn}/index/*",
+      "${aws_dynamodb_table.dynamodb.arn}/stream/*"
+    ]
+  }
+}
+
+
+policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Action = ["dynamodb:*"]
+      Effect = "Allow"
+      Sid    = "ddbPermissions"
+      Resource = [
+        aws_dynamodb_table.dynamodb.arn,
+        "${aws_dynamodb_table.dynamodb.arn}/index/*"
+      ]
+    }
+  ]
+})
--- tf/modules/ddb-extusers/outputs.tf
+++ tf/modules/ddb-extusers/outputs.tf
@@ -1,11 +1,11 @@
-output "ddb-extusers-arn" {
+output "dynamodb_arn" {
   value = aws_dynamodb_table.dynamodb.arn
 }
 
-output "ddb-extusers-name" {
+output "dynamodb_table_name" {
   value = aws_dynamodb_table.dynamodb.name
 }
 
-output "iam-access-ddb-role-arn" {
+output "iam_dynamodb_access_role_arn" {
   value = aws_iam_role.iam_access_ddb.arn
 }
