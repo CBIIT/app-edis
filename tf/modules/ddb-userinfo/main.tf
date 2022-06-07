2f03f8f0b9efbc6b576db6f133cfaa103d2f0156
polonskyy <yakov.polonsky@nih.gov>
2022-06-07 16:49:20 -0400

    added ddb table for vds users; make api gateway cache optional

---
+++ tf/modules/ddb-userinfo/main.tf
@@ -0,0 +1,39 @@
+
+resource "aws_dynamodb_table" "table" {
+  name           = "userinfo-${var.env}"
+  hash_key       = "NEDId"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+
+
+  attribute {
+    name = "NEDId"
+    type = "S"
+  }
+  attribute {
+    name = "NIHORGACRONYM"
+    type = "S"
+  }
+
+  global_secondary_index {
+    hash_key        = "NIHORGACRONYM"
+    range_key       = "NEDId"
+    name            = "icIndex"
+    projection_type = "ALL"
+    read_capacity   = 5
+    write_capacity  = 5
+  }
+}
+
+output "ddb-arn" {
+  value = aws_dynamodb_table.table.arn
+}
+
+output "ddb-name" {
+  value = aws_dynamodb_table.table.name
+}
+
+output "iam-access-ddb-role-arn" {
+  value = aws_iam_role.iam_access_ddb.arn
+}
