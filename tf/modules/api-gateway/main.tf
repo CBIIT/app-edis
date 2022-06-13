d5cc7dd31110059902f3a85389b869e246fa7e76
polonskyy <yakov.polonsky@nih.gov>
2022-06-13 09:11:34 -0400

    Reuse workflows as template

--- tf/modules/api-gateway/main.tf
+++ tf/modules/api-gateway/main.tf
@@ -74,7 +74,7 @@ resource "aws_api_gateway_stage" "api_gateway" {
   rest_api_id           = aws_api_gateway_rest_api.api_gateway.id
   stage_name            = var.env
   cache_cluster_enabled = var.cache_enabled
-  cache_cluster_size    = (var.cache_enabled) ? var.cache_size : ""
+  cache_cluster_size    = (var.cache_enabled) ? var.cache_size : null
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway.arn
     format          = "{ \"requestTime\":  \"$context.requestTime\", \"requestId\": \"$context.requestId\", \"httpMethod\": \"$context.httpMethod\", \"path\": \"$context.path\", \"resourcePath\": \"$context.resourcePath\", \"status\": $context.status, \"responseLatency\": $context.responseLatency, \"xrayTraceId\": \"$context.xrayTraceId\", \"integrationRequestId\": \"$context.integration.requestId\", \"functionResponseStatus\": \"$context.integration.status\", \"integrationLatency\": \"$context.integration.latency\", \"integrationServiceStatus\": \"$context.integration.integrationStatus\", \"authorizeResultStatus\": \"$context.authorize.status\", \"authorizerServiceStatus\": \"$context.authorizer.status\", \"authorizerLatency\": \"$context.authorizer.latency\", \"authorizerRequestId\": \"$context.authorizer.requestId\", \"ip\": \"$context.identity.sourceIp\", \"userAgent\": \"$context.identity.userAgent\", \"principalId\": \"$context.authorizer.principalId\", \"user\": \"$context.identity.user\" }"
