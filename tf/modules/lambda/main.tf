1858211e17a40cdda90f96a94bc3ceef19d66681
polonskyy <yakov.polonsky@nih.gov>
2022-06-30 09:57:09 -0400

    added lambda-prepare-s3-for-vds function

--- tf/modules/lambda/main.tf
+++ tf/modules/lambda/main.tf
@@ -24,6 +24,7 @@ resource "aws_lambda_function" "lambda" {
     Name = var.resource_tag_name
   }
   filename = var.file-name
+  source_code_hash = filebase64sha256("${var.file-name}")
   vpc_config {
     security_group_ids = var.security_group_ids
     subnet_ids         = var.subnet_ids
