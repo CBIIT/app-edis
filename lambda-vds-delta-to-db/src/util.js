0c6a7facdbcd821b0f9501fbf5cb3123656c1772
polonskyy <yakov.polonsky@nih.gov>
2022-07-07 10:35:59 -0400

    added lambda function to refresh dynamodb from S3 delta files

---
+++ lambda-vds-delta-to-db/src/util.js
@@ -0,0 +1,89 @@
+'use strict'
+
+const { conf } = require('./conf');
+const base64LdapFields = conf.vds.base64LdapFields;
+
+
+// add leading zero
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+
+/**
+ * Format a refresh timestamp as YYYYMMddHHmmss
+ * @param date  Date object
+ * @returns {string} formatted string
+ */
+function formatDate(date) {
+    return (
+        [
+            date.getFullYear(),
+            padTo2Digits(date.getMonth() + 1),
+            padTo2Digits(date.getDate()),
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
+        ].join('')
+    );
+}
+
+function getEmail(obj) {
+
+    let result = null;
+
+    const proxyEmails = obj.proxyAddresses;
+    if (proxyEmails) {
+        if (Array.isArray(proxyEmails)) {
+            proxyEmails.forEach(email => {
+                const data = email.split(':');
+                if (data[0] === 'SMTP') {
+                    result = data[1];
+                }
+            });
+        } else {
+            const data = proxyEmails.split(':');
+            if (data[0] === 'SMTP') {
+                result = data[1];
+            }
+        }
+    }
+    return result;
+};
+
+function getBuilding(obj) {
+
+    if (obj.BUILDINGNAME) {
+        return 'BG ' + obj.BUILDINGNAME;
+    } else {
+        return 'N/A';
+    }
+};
+
+function getDivision(obj) {
+
+    let result = 'N/A';
+
+    if (obj.NIHORGPATH) {
+        const orgPathArr = obj.NIHORGPATH.split(' ') || [];
+        const len = orgPathArr.length;
+
+        if (len > 0 && len <= 2) {
+            result = orgPathArr[len - 1];
+        } else if (len > 2) {
+            if (orgPathArr[1] === 'OD') {
+                result = orgPathArr[2];
+            } else {
+                result = orgPathArr[1];
+            }
+        }
+    }
+
+    return result;
+
+};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = { formatDate, getEmail, getBuilding, getDivision, sleep };
