9badabaeb883441516b0f58063628fd09f1e45f8
polonskyy <yakov.polonsky@nih.gov>
2022-08-31 10:02:44 -0400

    added generate-ts-swagger lambda function

--- tf/main.tf
+++ tf/main.tf
@@ -136,6 +136,44 @@ module "api-gateway-userinfo" {
   resource_tag_name   = "edis"
 }
 
+module "lambda-generate-ts-api" {
+  count = (var.build-userinfo) ? 1 : 0
+  source              = "./modules/lambda"
+  env                 = var.env
+  must-be-role-prefix = var.role-prefix
+  must-be-policy-arn  = var.policy-boundary-arn
+  resource_tag_name   = "edis"
+  region              = "us-east-1"
+  app                 = "edis"
+  lambda-name         = "generate-ts-api"
+  file-name           = "../lambda-zip/generate-ts-swagger.zip"
+  lambda-description  = "Lambda function contains NED REST APIs implementation using Typescript."
+  lambda-env-variables = tomap({
+    LOG_LEVEL = "info"
+    SECRET    = "era-commons-connect"
+  })
+  lambda-managed-policies        = { for idx, val in local.lambda_generate_ts_api_role_policies: idx => val }
+  create_api_gateway_integration = true
+  api_gateway_rest_api_id        = module.api-gateway-generate-ts[0].rest_api_id
+  subnet_ids = [ var.subnet1, var.subnet2 ]
+  security_group_ids = [ var.vpcsg ]
+}
+
+module "api-gateway-generate-ts" {
+  count = (var.build-userinfo) ? 1 : 0
+  source              = "./modules/api-gateway"
+  env                 = var.env
+  must-be-role-prefix = var.role-prefix
+  must-be-policy-arn  = var.policy-boundary-arn
+  okta-issuer         = lookup(local.tier_conf, var.env).issuer
+  app                 = "edis"
+  app-description     = "Enterprise Data & Integration Services Web Services for NED w Typescript"
+  api-swagger         = data.template_file.api_userinfo_swagger[0].rendered
+  api-resource-policy = local.era_commons_resource_policy
+  api-gateway-name    = "userinfo"
+  resource_tag_name   = "edis"
+}
+
 module "lambda-vds-users-delta" {
   count = (var.build-userinfo) ? 1 : 0
   source              = "./modules/lambda"
