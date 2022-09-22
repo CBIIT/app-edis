91779b4aa89c8f1895f87243cbaa478dce7d5b47
polonskyy <yakov.polonsky@nih.gov>
2022-09-22 10:29:59 -0400

    terraform fixes - include amazon integration inside swagger file

--- generate-ts-swagger/src/swagger.ts
+++ generate-ts-swagger/src/swagger.ts
@@ -4,6 +4,7 @@ import {AppConfig, LogLevel} from "ts-lambda-api";
 import {RestClient} from "typed-rest-client";
 import {HttpClientResponse} from "typed-rest-client/HttpClient";
 import * as fs from "fs";
+import * as yaml from "js-yaml"
 
 const args = process.argv.slice(2)
 console.log('ARGUMENTS:', args)
@@ -26,10 +27,23 @@ let httpClient = restClient.client;
 
 async function generate() {
     await app.runServer([])
-    let response: HttpClientResponse = await httpClient.get("http://localhost:8080/generatets/v1/open-api.yml")
+    let response: HttpClientResponse = await httpClient.get("http://localhost:8080/generatets/v1/open-api.json")
     let body = await response.readBody();
-    console.log('Response:', body)
-    fs.writeFileSync(path.join(__dirname, OUTPUT_PATH + "/generate-ts-swagger.yml"), body);
+    let jsonBody = JSON.parse(body);
+    Object.entries(jsonBody['paths']).forEach(([key, path]) => {
+        Object.entries(path).forEach(([key, method]) => {
+            //  We are in body.paths.<path>.<method>
+            method['x-amazon-apigateway-integration'] = {
+                uri: '${lambda_invoke_arn}',
+                httpMethod: 'POST',
+                timeoutInMillis: 29000,
+                type: 'aws_proxy'
+            }
+        });
+    });
+    console.log('Response:', jsonBody);
+    let yamlBody = yaml.dump(jsonBody);
+    fs.writeFileSync(path.join(__dirname, OUTPUT_PATH + "/generate-ts-swagger.yml"), yamlBody);
     await app.stopServer();
 }
 
