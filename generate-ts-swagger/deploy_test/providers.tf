3eded3631a4ecb3acf4138adb00e947b8368d088
polonskyy <yakov.polonsky@nih.gov>
2022-10-17 10:53:01 -0400

    testing github actions features

---
+++ generate-ts-swagger/deploy_test/providers.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+
+  # define s3 configurstion parameters in init cli command options
+  # like: terraform init -backend-config="bucket=<s3bucket>" -backend-config="key=api-edis-tf-state/api-edis-dev2" -backend-config="region=us-east-1"
+  backend "s3" {
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
