adc497456c4dcb9528e3f847a4b51786604f09e8
polonskyy <yakov.polonsky@nih.gov>
2022-10-17 11:07:40 -0400

    testing github actions features

--- generate-ts-swagger/deploy_test/main.tf
+++ generate-ts-swagger/deploy_test/main.tf
@@ -1,12 +1,12 @@
 
-resource "build" "swagger" {
+resource "null_resource" "swagger" {
   provisioner "local-exec" {
     command = "npm run swagger"
     working_dir = "../"
   }
 }
 
-resource "build" "lambda-zip" {
+resource "null_resource" "lambda-zip" {
   provisioner "local-exec" {
     command = "npm run zip-prod"
     working_dir = "../"
