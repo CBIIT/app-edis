57d6f69f236ae66999d6b3ae303e9355b19082c3
polonskyy <yakov.polonsky@nih.gov>
2023-04-07 09:35:24 -0400

    user api deployment refactoring - fixes

--- tf-userinfo/step.tf
+++ tf-userinfo/step.tf
@@ -241,7 +241,7 @@ resource "aws_sfn_state_machine" "edis_sfn_refresh_vds" {
       "Resource": "arn:aws:states:::lambda:invoke",
       "OutputPath": "$.Payload",
       "Parameters": {
-        "FunctionName": "${module.lambda-vds-users-delta.arn}:$LATEST"
+        "FunctionName": "${module.lambda-vds-users-delta.arn}:$LATEST",
         "Payload": {
           "DB_NAME": "vdsdb_dev",
           "S3SUBFOLDER": "vds",
