084b84b524ea3312b3941d6edf6c4bb3747dea30
polonskyy <yakov.polonsky@nih.gov>
2023-05-05 09:34:27 -0400

    added terraform deployment for era commons

---
+++ tf-eracommons/README.md
@@ -0,0 +1,65 @@
+# Enterprise Data & Integration Services Web Services
+
+## Terraform scripts deployment
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.14.9 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 4.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 4.22.0 |
+| <a name="provider_template"></a> [template](#provider\_template) | 2.2.0 |
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_api-gateway-era-commons"></a> [api-gateway-era-commons](#module\_api-gateway-era-commons) | ./modules/api-gateway | n/a |
+| <a name="module_api-gateway-userinfo"></a> [api-gateway-userinfo](#module\_api-gateway-userinfo) | ./modules/api-gateway | n/a |
+| <a name="module_ddb-extusers"></a> [ddb-extusers](#module\_ddb-extusers) | ./modules/ddb-extusers | n/a |
+| <a name="module_ddb-userinfo"></a> [ddb-userinfo](#module\_ddb-userinfo) | ./modules/ddb-userinfo | n/a |
+| <a name="module_lambda-era-commons-api"></a> [lambda-era-commons-api](#module\_lambda-era-commons-api) | ./modules/lambda | n/a |
+| <a name="module_lambda-load-from-vds"></a> [lambda-load-from-vds](#module\_lambda-load-from-vds) | ./modules/lambda | n/a |
+| <a name="module_lambda-prepare-s3-for-vds"></a> [lambda-prepare-s3-for-vds](#module\_lambda-prepare-s3-for-vds) | ./modules/lambda | n/a |
+| <a name="module_lambda-sqs-batch-to-db"></a> [lambda-sqs-batch-to-db](#module\_lambda-sqs-batch-to-db) | ./modules/lambda | n/a |
+| <a name="module_lambda-userinfo-api"></a> [lambda-userinfo-api](#module\_lambda-userinfo-api) | ./modules/lambda | n/a |
+| <a name="module_lambda-vds-delta-to-db"></a> [lambda-vds-delta-to-db](#module\_lambda-vds-delta-to-db) | ./modules/lambda | n/a |
+| <a name="module_lambda-vds-delta-to-sqs"></a> [lambda-vds-delta-to-sqs](#module\_lambda-vds-delta-to-sqs) | ./modules/lambda | n/a |
+| <a name="module_lambda-vds-users-delta"></a> [lambda-vds-users-delta](#module\_lambda-vds-users-delta) | ./modules/lambda | n/a |
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_iam_policy.iam_access_s3](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy) | resource |
+| [aws_iam_role.step_function](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_policy_document.assume_role_step_function](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [aws_iam_policy_document.iam_access_s3](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+| [template_file.api_era-commons-swagger](https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/file) | data source |
+| [template_file.api_userinfo_swagger](https://registry.terraform.io/providers/hashicorp/template/latest/docs/data-sources/file) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_build-eracommons"></a> [build-eracommons](#input\_build-eracommons) | Set to true to deploy era commons API related resources | `bool` | `false` | no |
+| <a name="input_build-userinfo"></a> [build-userinfo](#input\_build-userinfo) | Set to true to deploy user info API related resources | `bool` | `false` | no |
+| <a name="input_env"></a> [env](#input\_env) | Environment tier | `string` | `"dev"` | no |
+| <a name="input_policy-boundary-arn"></a> [policy-boundary-arn](#input\_policy-boundary-arn) | Must be policy to include in any IAM role | `string` | `""` | no |
+| <a name="input_role-prefix"></a> [role-prefix](#input\_role-prefix) | Must be prefix to any IAM role | `string` | `"power-user-edis"` | no |
+| <a name="input_s3bucket-for-vds-users"></a> [s3bucket-for-vds-users](#input\_s3bucket-for-vds-users) | S3 Bucket name to load users info from VDS | `string` | n/a | yes |
+| <a name="input_subnet1"></a> [subnet1](#input\_subnet1) | VPC Subnet 1 id for Lambda functions placed inside VPC | `string` | n/a | yes |
+| <a name="input_subnet2"></a> [subnet2](#input\_subnet2) | VPC Subnet 2 id for Lambda functions placed inside VPC | `string` | n/a | yes |
+| <a name="input_vpcsg"></a> [vpcsg](#input\_vpcsg) | Security Group id for Lambda functions placed inside VPC | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ tf-eracommons/main.tf
@@ -0,0 +1,116 @@
+
+module "ddb-extusers" {
+  source              = "./modules/ddb-extusers"
+  env                 = var.env
+  must-be-role-prefix = local.power-user-prefix
+  must-be-policy-arn  = local.policy-boundary-arn
+}
+
+module "lambda-era-commons-api" {
+  depends_on          = [module.ddb-extusers]
+  source              = "../tf-lib/modules/lambda"
+  env                 = var.env
+  must-be-role-prefix = local.power-user-prefix
+  must-be-policy-arn  = local.policy-boundary-arn
+  resource_tag_name   = "edis"
+  region              = "us-east-1"
+  app                 = "edis"
+  lambda-name         = "era-commons-api"
+  file-name           = abspath("../built-artifacts/lambda-userapi/out/lambda-eracommons-api.zip")
+  lambda-description  = "Lambda function contains eRA Commons External Users Info REST APIs implementation."
+  lambda-env-variables = tomap({
+    LOG_LEVEL = "info"
+    TABLE     = module.ddb-extusers.ddb-extusers-name
+  })
+  lambda-managed-policies        = { for idx, val in local.lambda_era_commons_api_role_policies: idx => val }
+  create_api_gateway_integration = true
+  api_gateway_rest_api_id        = module.api-gateway-era-commons.rest_api_id
+}
+
+module "api-gateway-era-commons" {
+  depends_on          = [module.ddb-extusers]
+  source              = "../tf-lib/modules/api-gateway"
+  env                 = var.env
+  must-be-role-prefix = local.power-user-prefix
+  must-be-policy-arn  = local.policy-boundary-arn
+  app                 = "edis"
+  app-description     = "Enterprise Data & Integration Services Web Services for eRA Commons users"
+  api-swagger         = data.template_file.api_era-commons-swagger.rendered
+  api-resource-policy = local.api_gateway_resource_policy
+  api-gateway-name    = "era-commons"
+  resource_tag_name   = "edis"
+}
+
+module "lambda-era-commons-auth" {
+  source              = "../tf-lib/modules/lambda"
+  env                 = var.env
+  must-be-role-prefix = local.power-user-prefix
+  must-be-policy-arn  = local.policy-boundary-arn
+  resource_tag_name   = "edis"
+  region              = "us-east-1"
+  app                 = "edis"
+  lambda-name         = "eracommons-auth"
+  file-name           = abspath("../built-artifacts/lambda-auth/out/lambda-auth.zip")
+  lambda-description  = "Lambda function to run Athena query to get VDS users delta for refresh."
+  lambda-env-variables = tomap({
+    LOG_LEVEL = "info"
+    "AUDIENCE"  = "api://default"
+    "ISSUER"    = lookup(local.tier_conf, var.env).issuer
+
+  })
+  lambda-managed-policies        = { for idx, val in local.lambda_era_commons_auth_policies: idx => val }
+  create_api_gateway_integration = false
+}
+
+resource "aws_lambda_permission" "_" {
+  principal     = "apigateway.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda-era-commons-auth.arn
+
+  source_arn = "arn:aws:execute-api:us-east-1:${
+    data.aws_caller_identity._.account_id
+    }:${
+    module.api-gateway-era-commons.rest_api_id
+  }/*"
+}
+
+module "lambda-eracommons" {
+  depends_on          = [module.ddb-extusers]
+  source              = "../tf-lib/modules/lambda"
+  env                 = var.env
+  must-be-role-prefix = local.power-user-prefix
+  must-be-policy-arn  = local.policy-boundary-arn
+  resource_tag_name   = "edis"
+  region              = "us-east-1"
+  app                 = "edis"
+  lambda-name         = "era-commons-refresh"
+  file-name           = abspath("../built-artifacts/lambda-eracommons/out/lambda-eracommons.zip")
+  lambda-description  = "Lambda function aceeses Oracle eRA Commons and refreshes Dynamo DB table."
+  lambda-env-variables = tomap({
+    LOG_LEVEL = "info"
+    SECRET    = "era-commons-connect-${var.env}"
+    TABLE     = module.ddb-extusers.ddb-extusers-name
+  })
+  lambda-managed-policies        = { for idx, val in local.lambda_era_commons_role_policies: idx => val }
+  lambda-layers = local.lambda-layers
+  subnet_ids = [ var.subnet1, var.subnet2 ]
+  security_group_ids = [ var.vpcsg ]
+}
+
+resource "aws_cloudwatch_event_rule" "edis_refresh_eracommons" {
+  name = "edis-eracommons-refresh-${var.env}"
+  description = "Start Lambda Function to refresh eRA Commons user data"
+  schedule_expression = "cron(0 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "edis_refresh_eracommons" {
+  arn  = module.lambda-eracommons.arn
+  rule = aws_cloudwatch_event_rule.edis_refresh_eracommons.name
+}
+
+resource "aws_lambda_permission" "edis_refresh_eracommons" {
+  principal     = "events.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+  function_name = module.lambda-eracommons.name
+  source_arn = aws_cloudwatch_event_rule.edis_refresh_eracommons.arn 
+}
