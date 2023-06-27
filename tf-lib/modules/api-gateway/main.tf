6f2fc4fc14d451e3a0a0fdf931083883d36a090f
polonskyy <yakov.polonsky@nih.gov>
2023-06-27 13:54:19 -0400

    removed "Stage-snapshot" deployment tier of API Gateway

--- tf-lib/modules/api-gateway/main.tf
+++ tf-lib/modules/api-gateway/main.tf
@@ -20,7 +20,6 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 
 resource "aws_api_gateway_deployment" "api_gateway" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  stage_name = "Stage-snapshot"
 }
 
 resource "aws_api_gateway_rest_api_policy" "api_gateway" {
