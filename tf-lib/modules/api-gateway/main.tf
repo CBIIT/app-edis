e7eef085d95c0781b7f6f24095622861e61e0bcf
polonskyy <yakov.polonsky@nih.gov>
2023-09-27 14:56:13 -0400

    redeploy api gateway on yaml change

--- tf-lib/modules/api-gateway/main.tf
+++ tf-lib/modules/api-gateway/main.tf
@@ -20,6 +20,11 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 
 resource "aws_api_gateway_deployment" "api_gateway" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_gateway.body))
+  }
+
   lifecycle {
     create_before_destroy = true
   }
