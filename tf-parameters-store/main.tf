8d64fe58a2ea31ad7dfb26a83681635245e0c376
polonskyy <yakov.polonsky@nih.gov>
2024-02-20 16:56:22 -0500

    Added set Parameters Store entries for the given app tier

---
+++ tf-parameters-store/main.tf
@@ -0,0 +1,16 @@
+
+resource "aws_ssm_parameter" "eadis" {
+  for_each = local.parameter_pairs
+  name  = "${var.env}/app/eadis/${each.key}"
+  type  = "String"
+  value = each.value
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "secure_eadis" {
+  for_each = local.secure_parameter_pairs
+  name  = "${var.env}/app/eadis/${each.key}"
+  type  = "SecureString"
+  value = each.value
+  overwrite = true
+}
