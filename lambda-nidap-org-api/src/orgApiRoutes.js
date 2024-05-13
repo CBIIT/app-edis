5dc3ec27e3be9355484f7f6946407916172921fc
polonskyy <yakov.polonsky@nih.gov>
2024-05-13 14:58:51 -0400

    Added search orgs by sac and search orgs which sac starts with prefix

--- lambda-nidap-org-api/src/orgApiRoutes.js
+++ lambda-nidap-org-api/src/orgApiRoutes.js
@@ -17,6 +17,36 @@ function orgRoutes(app, opts) {
             res.status(500).send(error);
         }
     });
+    app.get('/orgs/:nihsac', async (req, res) => {
+        console.info(opts.prefix + '/orgs', req.params);
+        try {
+            if (req.query.Testing) {
+                console.info(`Return in Testing mode`);
+                return { 'Success': true};
+            }
+            const result = await searchOrgBySac(rec.params.nihsac);
+            if (result.status) {
+                res.status(result.status).send(result.error);
+            }
+            else {
+                res.json(result);
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+    app.get('/orgTree/:nihsac', async (req, res) => {
+        console.info(opts.prefix + '/orgs', req.params);
+        try {
+            if (req.query.Testing) {
+                console.info(`Return in Testing mode`);
+                return { 'Success': true};
+            }
+            res.json(await searchOrgTreeBySac(req.params.nihsac));
+        } catch (error) {
+            res.status(error.response.status).send(error.data);
+        }
+    });
     // app.get('/userByNIHid/:id', async (req, res) => {
     //     console.info('/userapi/ned/userByNIHid', req.params);
     //     try {
@@ -82,7 +112,7 @@ async function listAllOrgs(nextPageToken) {
     console.info(`List All Organizations from NIDAP`);
     let URL = `${conf.nidap.url_v1}objects/${conf.nidap.ontology_org}?orderBy=properties.sac:asc`;
     if (nextPageToken) {
-        URL += `?nextPageToken=${nextPageToken}`;
+        URL += `&nextPageToken=${nextPageToken}`;
     }
     const resp = await axios.get(URL, {
         headers: {
@@ -110,4 +140,81 @@ async function listAllOrgs(nextPageToken) {
     return result;
 }
 
-module.exports = { orgRoutes, listAllOrgs};
+async function searchOrgBySac(sac) {
+    console.info(`Search Organizations by SAC code`);
+    let URL = `${conf.nidap.url_v1}objects/${conf.nidap.ontology_org}/${sac}`;
+    const resp = await axios.get(URL, {
+        headers: {
+            'Authorization': `Bearer ${conf.nidap.auth_token}`
+        }
+    });
+    if (resp.status === 200) { //Not found
+        const p = resp.data.properties;
+        const result = new Organization(p.sac, p.organizationAcronym, p.organization, p.organizationPath,
+            p.instituteAcronym, p.instiute, p.parentSac, p.docSac, p.docOrganizationPath);
+        console.debug(`Got result `);
+        console.debug(result);
+        return result;
+    }
+    else if (resp.status == 404) { // Not Found
+        console.info(`Got result - Not found - ${resp.data}`);
+        return new Organization();
+    }
+
+    const err_resp = {
+        status: resp.status,
+        error: resp.statusText
+    }
+
+    console.debug(`Got error result ${err_resp}`);
+    return err_resp;
+}
+
+async function searchOrgTreeBySac(sac, nextPageToken) {
+    console.info(`Search Organizations Subtree from NIDAP for the givan SAC Code ${sac}`);
+    let URL = `${conf.nidap.url_v1}objects/${conf.nidap.ontology_org}/search`;
+
+    const data = {
+        query: {
+            type: "prefix",
+            field: "properties.sac",
+            value: sac
+        },
+        orderBy: {
+            fields: [
+                {
+                    field: "properties.sac",
+                    direction: "asc"
+                }
+            ]
+        }
+    }
+    if (nextPageToken) {
+        data['pageToken'] = nextPageToken;
+    }
+    const resp = await axios.post(URL, data, {
+            headers: {
+                'Authorization': `Bearer ${conf.nidap.auth_token}`
+            },
+    });
+
+    const nidapResp = resp.data;
+    // console.debug(nidapResp);
+    const result = new OrgListResponse();
+    result.count = nidapResp.data.length;
+    if (nidapResp.nextPageToken) {
+        result.lastEvaluatedKey = nidapResp.nextPageToken;
+    }
+    nidapResp.data.forEach((r) => {
+        const p = r.properties;
+        result.items.push(new Organization(p.sac, p.organizationAcronym, p.organization, p.organizationPath,
+            p.instituteAcronym, p.instiute, p.parentSac, p.docSac, p.docOrganizationPath));
+    })
+    // Post-process NIDAP response
+    // console.debug(`Got result `);
+    // console.debug(result);
+    return result;
+}
+
+module.exports = { orgRoutes, listAllOrgs, searchOrgBySac, searchOrgTreeBySac};
+
