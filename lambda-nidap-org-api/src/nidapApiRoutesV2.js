ab41ca749dd19efcfab5e6d1c6a98469a0b36c44
polonskyy <yakov.polonsky@nih.gov>
2024-11-26 11:41:31 -0500

    added generic dataapi/nidap - get preconfigured data from NIDAP datasource

---
+++ lambda-nidap-org-api/src/nidapApiRoutesV2.js
@@ -0,0 +1,99 @@
+const {conf} = require("./conf");
+const {getAuthorizationHeader, processPaginatedResult} = require("./util");
+const axios = require("axios");
+
+function nidapApiRoutesV2(app, opts) {
+    app.get('/:dataObject', async (req, res) => {
+        console.info(opts.prefix + '/' + req.params['dataObject']);
+        try {
+            if (req.query['Testing']) {
+                console.info(`Return in Testing mode`);
+                res.json({ 'Success': true});
+            }
+            res.json(await listAllData(req.params['dataObject'], req.query.lastEvaluatedKey));
+        } catch (error) {
+            console.error('ERROR:', error);
+            res.status(500).send(error);
+        }
+    });
+    app.get('/:dataObject/:primary', async (req, res) => {
+        console.info(`${opts.prefix} / ${req.params['dataObject']} / ${req.params['primary']}`);
+        try {
+            if (req.query['Testing']) {
+                console.info(`Return in Testing mode`);
+                res.json({ 'Success': true});
+            }
+            res.json(await searchDataByPrimaryKey(req.params['dataObject'], req.params['primary']));
+        } catch (error) {
+            console.error('ERROR:', error);
+            res.status(500).send(error);
+        }
+    });
+}
+
+async function listAllData(dataType, lastEvaluatedKey) {
+    console.debug(`List All ${dataType} from NIDAP`);
+    confData = conf.nidap[dataType];
+    if ('undefined' === typeof confData) {
+        throw new Error('The object type ' + dataType + ' is not configured')
+    }
+    const ontology = confData['ontology'] || conf.nidap.default_ontology;
+    let queryParameters = (confData['orderBy']) ? '?orderBy=' + confData['orderBy'] : '';
+    if (lastEvaluatedKey) {
+        queryParameters += ((queryParameters.length > 0) ? '&' : '?') + `pageToken=${lastEvaluatedKey}`;
+    }
+    const objectType = confData['ontology_object_type'];
+    let url = `${conf.nidap.url_v2}${ontology}/objects/${objectType}${queryParameters}`;
+    console.info(`URL: ${url}`);
+    const auth = await getAuthorizationHeader(conf.nidap.auth_type, conf.nidap.auth_token, conf.nidap.auth_client_id,
+        conf.nidap.auth_client_secret, conf.nidap.url_token);
+    const resp = await axios.get(url, {
+        headers: {
+            'Authorization': auth
+        }
+    });
+    const items = [];
+    const propertiesMap = (confData['propertiesMap']) ? JSON.parse(confData['propertiesMap']) : null;
+    resp.data.data.forEach((r) => {
+        items.push(migrateProperties(r, propertiesMap));
+    });
+    const result = processPaginatedResult(resp.data, items);
+    console.info(`listAllData completed.  Returns ${result.items.length} items`);
+    return result;
+}
+
+async function searchDataByPrimaryKey(dataType, primaryKey) {
+    console.debug(`Search ${dataType} from NIDAP by ${primaryKey}`);
+    confData = conf.nidap[dataType];
+    if ('undefined' === typeof confData) {
+        throw new Error('The object type ' + dataType + ' is not configured')
+    }
+    const ontology = confData['ontology'] || conf.nidap.default_ontology;
+    const objectType = confData['ontology_object_type'];
+    let url = `${conf.nidap.url_v2}${ontology}/objects/${objectType}/${primaryKey}`;
+    console.info(`URL: ${url}`);
+    const auth = await getAuthorizationHeader(conf.nidap.auth_type, conf.nidap.auth_token, conf.nidap.auth_client_id,
+        conf.nidap.auth_client_secret, conf.nidap.url_token);
+    const resp = await axios.get(url, {
+        headers: {
+            'Authorization': auth
+        }
+    });
+    const propertiesMap = (confData['propertiesMap']) ? JSON.parse(confData['propertiesMap']) : null;
+    return migrateProperties(resp.data, propertiesMap);
+}
+
+function migrateProperties(r, map) {
+    if (map === null) {
+        return r;
+    }
+    const ret = {};
+    for (const key in map) {
+        if (r[map[key]]) {
+            ret[key] = r[map[key]];
+        }
+    }
+    return ret;
+}
+
+module.exports = { nidapApiRoutesV2, listAllData, searchDataByPrimaryKey };
---
+++ lambda-nidap-org-api/src/util.js
@@ -0,0 +1,54 @@
+const {conf} = require("./conf");
+const axios = require("axios");
+const {OrgListResponse, Organization} = require("./orgListResponse");
+const {DataListResponse} = require("./dataListResponse");
+
+async function getAuthorizationHeader(auth_type, auth_token, client_id, client_secret, get_token_url) {
+    if (auth_type != 'prod') {
+        return 'Bearer ' + auth_token;
+    }
+
+    // Get Auth2 token implementation
+    const data = {
+        grant_type: 'client_credentials',
+        client_id: client_id,
+        client_secret: client_secret
+    };
+    const resp = await axios.post(get_token_url, data, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+    console.debug('Token response', resp.data);
+    return 'Bearer ' + resp.data['access_token'];
+}
+
+function processPaginatedResult(response, items) {
+    const result = new DataListResponse();
+    result.count = response.data.length;
+    if (response.nextPageToken) {
+        result.lastEvaluatedKey = response.nextPageToken;
+    }
+    result.items = items;
+    return result;
+}
+
+function convertParametersToJson(data, prefix) {
+    const result = {};
+    data.Parameters.forEach((p) => {
+        let paths = p.Name.slice(prefix.length).split('/');
+        let tempResult = result;
+        for (let i = 0; i < paths.length - 1; i++) {
+            if (typeof tempResult[paths[i]] === 'undefined') {
+                tempResult[paths[i]] = {};
+            }
+            tempResult = tempResult[paths[i]];
+        }
+        tempResult[paths[paths.length - 1]] = p.Value;
+    });
+    console.debug('Parameters:', result);
+    return result;
+}
+
+
+module.exports = { getAuthorizationHeader, processPaginatedResult, convertParametersToJson };
