afcb987b50321771220e4192cb1c673b7dc0d1a8
gmomplaisir <6799317+gmomplaisir@users.noreply.github.com>
2024-12-18 13:36:58 -0500

    Initial source for the Frederick Property API

---
+++ lambda-load-from-fred-props/src/util.js
@@ -0,0 +1,46 @@
+'use strict'
+
+function convertForDynamoDB(row) {
+    const res = {}
+    for (const p in row) {
+        const newProp = p; //TODO -- add if you need to map to a new property
+        if (row[p] == null) {
+            res[newProp] = null;
+        }
+        else if (typeof(row[p]) === 'number') {
+            res[newProp] = row[p];
+        }
+        else if (row[p] instanceof Date) {
+            res[newProp] = formatDate(row[p]);
+        } else {
+            res[newProp] = cleanString(row[p]);
+        }
+    }
+    res['USER_ID'] = '' + ((res['USER_ID'] === undefined) ? 'UNKNOWN' : res['USER_ID']) //convert to string
+
+    return res;
+}
+
+// add leading zero
+function padTo2Digits(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDate(date) {
+    return date.toISOString();
+}
+
+function cleanString(str) {
+    if (str.indexOf('\t') >= 0) {
+        console.log('string with tab: ', str )
+    }
+    let ret = str.replace(/"/g, '\\"');
+    return ret.replace(/\u0009/g, " ");
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = { convertForDynamoDB, sleep };
